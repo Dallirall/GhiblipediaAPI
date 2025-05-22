f03ca2e9cb1e0a2c7b7d1cb8d3b728c64edc891f
Matilda <matildafahlstrom90@hotmail.com>
2025-05-22 09:37:48 +0200

    Justerat models till den fixade databasen

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -4,13 +4,12 @@
     {
         public int Movie_id { get; set; }
         public string English_title { get; set; }
-        public string? Japanese_title { get; set; }
-        public string? Romaji_title { get; set; }
+        public string? Japanese_title { get; set; }        
         public int? Release_year { get; set; }
         public string? Image_url { get; set; }
         public string? Trailer_url { get; set; }
-        public string? Description_short { get; set; }
-        public string? Description_long { get; set; }
+        public string? Summary { get; set; }
+        public string? Plot { get; set; }
         public string? Director { get; set; }
         public string? Genre { get; set; }
         public int? Running_time_mins { get; set; }
