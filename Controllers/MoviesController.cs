d4a634bdcd2b90209ff861df583b5d564f655847
Matilda <matildafahlstrom90@hotmail.com>
2025-05-23 11:52:31 +0200

    Håller på med GetMovieSpecificFields()

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -16,6 +16,7 @@ namespace GhiblipediaAPI.Controllers
             _movieRepo = movieRepo;
         }
 
+        //TODO: Gör det async 
         [HttpGet]
         public ActionResult<IEnumerable<Movie>> Get()
         {
@@ -28,11 +29,8 @@ namespace GhiblipediaAPI.Controllers
         {
             var movie = _movieRepo.GetMovieById(movie_id);
 
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
+            if (movie == null) return NotFound();
+            
             return Ok(movie);
         }
 
@@ -41,12 +39,51 @@ namespace GhiblipediaAPI.Controllers
         {
             var movie = _movieRepo.GetMovieByTitle(english_title);
 
-            if (movie == null)
-            {
-                return NotFound();
-            }
+            if (movie == null) return NotFound();
 
             return Ok(movie);
+        }
+
+        [HttpGet("title={english_title}")] //ta reda på hur jag fixar detta fel
+        public IActionResult GetMovieSpecificFields(string english_title, [FromQuery] string fields)
+        {
+            var movie = _movieRepo.GetMovieByTitle(english_title);
+            if (movie == null) return NotFound();            
+
+            var fieldList = fields?.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            var result = new Dictionary<string, object>();
+            foreach (var field in fieldList)
+            {
+                switch (field.Trim().ToLower())
+                {
+                    case "movie_id":
+                        result["movie_id"] = movie.Movie_id; break;
+                    case "english_title":
+                        result["english_title"] = movie.English_title; break;
+                    case "japanese_title":
+                        result["japanese_title"] = movie.Japanese_title; break;
+                    case "release_year":
+                        result["release_year"] = movie.Release_year; break;
+                    case "image_url":
+                        result["image_url"] = movie.Image_url; break;
+                    case "trailer_url":
+                        result["trailer_url"] = movie.Trailer_url; break;
+                    case "summary":
+                        result["summary"] = movie.Summary; break;
+                    case "plot":
+                        result["plot"] = movie.Plot; break;
+                    case "director":
+                        result["director"] = movie.Director; break;
+                    case "genre":
+                        result["genre"] = movie.Genre; break;
+                    case "running_time_mins":
+                        result["running_time_mins"] = movie.Running_time_mins; break;
+                    default: break;
+                }
+            }
+
+            return Ok(result);
         }
 
         [HttpPost]
