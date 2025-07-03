170c32e85620ec0312d86ae2ac057d93da750869
Matilda <matildafahlstrom90@hotmail.com>
2025-07-03 11:19:00 +0200

    LÖST: Hade glömt ställa in Program.cs att kolla efter connectionstring i appsettings.Local, och har ändrat connstring till session pooler på supabase.

--- Program.cs
+++ Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddControllers();
 
